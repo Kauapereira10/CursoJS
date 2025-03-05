2834f10bc2649def2e2ae7f43a846a44031d15bf
Kaua Pereira <borgeskaua1210@gmail.com>
2025-03-05 15:29:35 -0300

    atualizado

---
+++ aula023/index.js
@@ -0,0 +1,22 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true && false -> vai retornar "o valor verdadeiro"
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+
+// console.log('Luiz' && null && 'Maria');
+
+// function falaOi() {
+//     return 'Oi';
+// }
+
+// let vaiExecutar = '22';
+// console.log(vaiExecutar && falaOi());
+// console.log(0 || false || null || 'Luiz Otavio' || true);
+ 
