a4054df59e239bf1391aca36e4b60a36587bafa8
Kaua Pereira <borgeskaua1210@gmail.com>
2025-03-13 14:49:12 -0300

    atualizar

--- aula020/js/script.js
+++ aula020/js/script.js
@@ -4,26 +4,43 @@ function meuEscopo () {
 
     const pessoas = [];
 
-    function recebeEventoForm (evento) {
-        evento.preventDefault();
+    function recebeEvento (event) {
+        event.preventDefault();
+
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
+        // Se a condição for verdadeira (ou seja, se pelo menos um dos campos estiver vazio), o código dentro do bloco if será executado.
+        if (!nome.value || !sobrenome.value || !peso.value || !altura.value) {
+            alert('Por favor, preencha todos os campos.');
+            return;
+        }
+
         pessoas.push({
             nome: nome.value,
             sobrenome: sobrenome.value,
             peso: peso.value,
-            altura: altura.value
+            altura: altura.value,
         });
+
         console.log(pessoas);
 
-        resultado.innerHTML = `<p>${nome.value} ${sobrenome.value} tem ${peso.value} kg com ${altura.value}.</p>`
+        // Limpar os campos do formulário
+        nome.value = '';
+        sobrenome.value = '';
+        peso.value = '';
+        altura.value = '';
 
+
+        // Exibir todos os resultados
+        resultado.innerHTML = pessoas.map(pessoa => 
+            `<p>${pessoa.nome} ${pessoa.sobrenome} tem ${pessoa.peso} kg com ${pessoa.altura} m.</p>`
+        ).join('');
     }
-    
-    form.addEventListener('submit', recebeEventoForm);
+
+    form.addEventListener('submit', recebeEvento);
     
 }
 meuEscopo();
